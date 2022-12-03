8b3c897aa0187d926289ddfff47283d6a2e11240
Nick Hynes <nhynes@nhynes.com>
2022-12-03 17:21:37 +0800

    backend: export ABIs

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,10 +1,49 @@
-import { HardhatUserConfig } from 'hardhat/config';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import canonicalize from 'canonicalize';
+import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
+import { HardhatUserConfig, task } from 'hardhat/config';
 
 import '@oasisprotocol/sapphire-hardhat';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-watcher';
 import 'solidity-coverage';
+
+const TASK_EXPORT_ABIS = 'export-abis';
+
+task(TASK_COMPILE, async (_args, hre, runSuper) => {
+  await runSuper();
+  await hre.run(TASK_EXPORT_ABIS);
+});
+
+task(TASK_EXPORT_ABIS, async (_args, hre) => {
+  const srcDir = path.basename(hre.config.paths.sources);
+  const outDir = path.join(hre.config.paths.root, 'abis');
+
+  const [artifactNames] = await Promise.all([
+    hre.artifacts.getAllFullyQualifiedNames(),
+    fs.mkdir(outDir, { recursive: true }),
+  ]);
+
+  await Promise.all(
+    artifactNames.map(async (fqn) => {
+      const { abi, contractName, sourceName } =
+        await hre.artifacts.readArtifact(fqn);
+      if (
+        abi.length === 0 ||
+        !sourceName.startsWith(srcDir) ||
+        contractName.endsWith('Test')
+      )
+        return;
+      await fs.writeFile(
+        `${path.join(outDir, contractName)}.json`,
+        `${canonicalize(abi)}\n`,
+      );
+    }),
+  );
+});
 
 const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
 
